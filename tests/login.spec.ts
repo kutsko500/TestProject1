e5ee1b65a95abca7225d21a382b17ee1e7759ea9
kutsko500 <kutsko500@gmail.com>
2026-02-25 11:15:41 -0600

    update log in and password setup

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,23 @@
 import { test, expect } from '@playwright/test';
 // 1. IMPORT: This tells this file where your "Remote Control" is hiding
 import { LoginPage } from '../pages/LoginPage'; 
-import testData from '../data/testData.json'
+
 test('should log in to the secure area', async ({ page }) => {
   // 2. THE HANDSHAKE: We "pick up" the remote and give it the browser tab (page)
   const loginPage = new LoginPage(page);
 
   // 3. THE ACTIONS: Instead of typing URLs and IDs, we just call our "Skills"
   await loginPage.visit(); 
-  await loginPage.loginUser(testData.user1.username, testData.user1.password);
+
+  /**
+   * THE SECRET GRAB:
+   * Instead of using a JSON file, we grab from the computer's "Environment".
+   * The '!' tells TypeScript: "I promise these aren't empty!"
+   */
+  const username = process.env.USER_NAME!;
+  const password = process.env.USER_PASS!;
+
+  await loginPage.loginUser(username, password);
 
   // 4. THE JUDGE: We check if the "Mission" was successful
   // We check if the URL now says "secure"
