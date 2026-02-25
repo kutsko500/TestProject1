7ce8adb076a04bc4d710aa0cad52bb0ddba9bc65
kutsko500 <kutsko500@gmail.com>
2026-02-25 10:27:18 -0600

    Clean up unused files

--- tests/practice.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('login test', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/login');
-  await page.fill('#username', 'tomsmith');
-  await page.fill('#password', 'SuperSecretPassword!');
-  await page.click('button[type="submit"]');
-  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
-});
